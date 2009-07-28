7c0f08c4840d5982f61fd3980a4e122701944c2c
glaforge <glaforge@gmail.com>
2009-07-29 00:57:47 +0200

    Further execution checks.

--- war/WEB-INF/groovy/executor.groovy
+++ war/WEB-INF/groovy/executor.groovy
@@ -8,9 +8,28 @@ def binding = new Binding([out: new PrintWriter(output)])
 def stacktrace = new StringWriter()
 def errWriter = new PrintWriter(stacktrace)
 
+class NoGaeSdkAccessGCL extends GroovyClassLoader {
+    Class loadClass(final String name, boolean lookupScriptFiles, boolean preferClassOverScript, boolean resolve) {
+        if (name.startsWith('com.google.appengine.'))
+            throw new SecurityException("Access to $name forbidden. You're not allowed to use the App Engine SDK within the console.")
+        super.loadClass(name, lookupScriptFiles, preferClassOverScript, resolve)
+    }
+}
+
+def gcl = new NoGaeSdkAccessGCL()
+
+def originalAsType = String.metaClass.metaMethods.find { it.name == 'asType' }
+String.metaClass.asType = { Class c ->
+    if (delegate.startsWith('com.google.appengine.')) {
+        throw new SecurityException("Access to $delegate forbidden. You're not allowed to use the App Engine SDK within the console.")
+    } else {
+        return originalAsType.doMethodInvoke(delegate, c)
+    }
+}
+
 def result = ""
 try {
-	result = new GroovyShell(binding).evaluate(scriptText)
+	result = new GroovyShell(gcl, binding).evaluate(scriptText)
 } catch (MultipleCompilationErrorsException e) {
 	stacktrace.append(e.message - 'startup failed, Script1.groovy: ')
 } catch (Throwable t) {
