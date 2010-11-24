abf8c1255b6266ff7af02b23a199de1c691808ac
glaforge <glaforge@gmail.com>
2010-11-24 16:50:54 +0100

    Reformating of routes

--- war/WEB-INF/routes.groovy
+++ war/WEB-INF/routes.groovy
@@ -1,15 +1,15 @@
-get "/script/@id",      forward: "/view.groovy?id=@id",     validate: { id ==~ /\d+/ }, cache: 24.hours
-get "/edit/@id",        forward: "/editscript.groovy?id=@id", validate: { id ==~ /\d+/ }
+get "/script/@id",      forward: "/view.groovy?id=@id",         validate: { id ==~ /\d+/ }, cache: 24.hours
+get "/edit/@id",        forward: "/editscript.groovy?id=@id",   validate: { id ==~ /\d+/ }
 
-get "/author/@author",  forward: "/recentscripts.gtpl?author=@author&limit=100",        cache: 5.seconds
+get "/author/@author",  forward: "/recentscripts.gtpl?author=@author&limit=100",            cache: 5.seconds
 
-get "/tag/@tag",        forward: "/recentscripts.gtpl?tag=@tag&limit=100",              cache: 5.seconds
+get "/tag/@tag",        forward: "/recentscripts.gtpl?tag=@tag&limit=100",                  cache: 5.seconds
 
-get "/scripts",         forward: "/recentscripts.gtpl?limit=100",                       cache: 5.seconds
-get "/scripts/@limit",  forward: "/recentscripts.gtpl?limit=@limit",                    cache: 5.seconds
+get "/scripts",         forward: "/recentscripts.gtpl?limit=100",                           cache: 5.seconds
+get "/scripts/@limit",  forward: "/recentscripts.gtpl?limit=@limit",                        cache: 5.seconds
 
-get "/badcaptcha",      forward: "/badcaptcha.gtpl",                                    cache: 24.hours
+get "/badcaptcha",      forward: "/badcaptcha.gtpl",                                        cache: 24.hours
 
-get "/nosuchscript",    forward: "/nosuchscript.gtpl",                                  cache: 24.hours
+get "/nosuchscript",    forward: "/nosuchscript.gtpl",                                      cache: 24.hours
 
-get "/about",           forward: "/WEB-INF/includes/about.gtpl",                        cache: 5.seconds
+get "/about",           forward: "/WEB-INF/includes/about.gtpl",                            cache: 5.seconds
