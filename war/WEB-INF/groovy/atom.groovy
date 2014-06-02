a6d70f669d5c36fae6b35d36866fb01d6f9b88f4
Guillaume Laforge <glaforge@gmail.com>
2014-06-02 10:54:29 +0200

    Output script content in the ATOM feed

--- war/WEB-INF/groovy/atom.groovy
+++ war/WEB-INF/groovy/atom.groovy
@@ -11,15 +11,14 @@ PreparedQuery preparedQuery = datastore.prepare(query)
 def entities = preparedQuery.asList(withLimit(10))
 
 def isoTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
-def prettyDate = new SimpleDateFormat("EEE, d MMM yyyy", Locale.US)
 
 response.contentType = "application/atom+xml;charset=utf-8"
 
-def mkp = new MarkupBuilder(out)
+def builder = new MarkupBuilder(out)
 
-//mkp.yieldUnescaped '''<?xml version="1.0" encoding="utf-8"?>'''
+//builder.yieldUnescaped '''<?xml version="1.0" encoding="utf-8"?>'''
 
-mkp.feed(xmlns: "http://www.w3.org/2005/Atom") {
+builder.feed(xmlns: "http://www.w3.org/2005/Atom") {
     title "Groovy Web Console for the Groovy Programming Language"
     subtitle "Sharing Groovy programming language snippets, one script at a time"
     link href: "http://groovyconsole.appspot.com", rel: "self"
@@ -28,7 +27,7 @@ mkp.feed(xmlns: "http://www.w3.org/2005/Atom") {
         name "Guillaume Laforge"
         email "glaforge@gmail.com"
     }
-    generator(uri: "http://gaelyk.appspot.com", version: "0.3.2", "Gaelyk lightweight Groovy toolkit for Google App Engine")
+    generator(uri: "http://gaelyk.appspot.com", version: "2.1.2", "Gaelyk lightweight Groovy toolkit for Google App Engine")
 
     entities.each { entity ->
         def authorText = entity.author && entity.author != 'Anonymous' ? entity.author : 'Anonymous'
@@ -38,9 +37,11 @@ mkp.feed(xmlns: "http://www.w3.org/2005/Atom") {
             title titleText
             link href: "http://groovyconsole.appspot.com/script/${entity.key.id}"
             updated isoTime.format(entity.dateCreated)
-            summary "Groovy programming language snippet posted by ${authorText} on the Groovy Web Console on ${prettyDate.format(entity.dateCreated)}."
             author {
                 name authorText
+            }
+            content(type: 'html') {
+                mkp.yield entity.script
             }
         }
     }
