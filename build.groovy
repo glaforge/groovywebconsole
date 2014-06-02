268f2edc0379d13c9f1238a33b2de093ce609c80
Guillaume Laforge <glaforge@gmail.com>
2014-06-02 09:48:07 +0200

    Upgrade to Groovy 2.3.2, with latest Gaelyk 2.1.2 snapshot and latest 1.9.5 App Engine SDK

--- build.groovy
+++ build.groovy
@@ -1,14 +1,14 @@
 def ant = new AntBuilder().sequential {
-	webinf = "war/WEB-INF"
-	taskdef name: "groovyc", classname: "org.codehaus.groovy.ant.Groovyc"
-	groovyc srcdir: "src", destdir: "${webinf}/classes", {
-		classpath {
-			fileset dir: "${webinf}/lib", {
-		    	include name: "*.jar"
-			}
-			pathelement path: "${webinf}/classes"
-		}
-		javac source: "1.5", target: "1.5", debug: "on"
-	}
+    webinf = "war/WEB-INF"
+    taskdef name: "groovyc", classname: "org.codehaus.groovy.ant.Groovyc"
+    groovyc srcdir: "src", destdir: "${webinf}/classes", {
+        classpath {
+            fileset dir: "${webinf}/lib", {
+                include name: "*.jar"
+            }
+            pathelement path: "${webinf}/classes"
+        }
+        javac source: "1.7", target: "1.7", debug: "on"
+    }
 }
 
