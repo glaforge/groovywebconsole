4f9fd359f0031b8a2ecfd0029831413b3bbc7f00
Guillaume Laforge <glaforge@gmail.com>
2015-08-06 17:46:31 +0200

    remove debugging logging

--- war/WEB-INF/groovy/view.groovy
+++ war/WEB-INF/groovy/view.groovy
@@ -1,7 +1,6 @@
 import com.google.appengine.api.datastore.Entity
 
 if (params.id) {
-    log.info "Params: $params.id"
     def id = Long.parseLong(params['id'])
 
     try {
@@ -12,11 +11,9 @@ if (params.id) {
         request.script = script
         forward params['embed'] ? 'embed.gtpl' : 'view.gtpl'
     } catch (Throwable t) {
-        log.info t.message
         t.printStackTrace()
         forward 'nosuchscript.gtpl'
     }
 } else {
-    log.info "No params.id = $params.id"
     forward 'nosuchscript.gtpl'
 }
