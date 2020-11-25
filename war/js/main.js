7c5a780c6404af9630ca7f50158b3f2a2865b85f
Deleplace <deleplace2015@gmail.com>
2020-11-25 15:14:16 +0100

    issues/33 Run button on published code page.

--- war/js/main.js
+++ war/js/main.js
@@ -40,54 +40,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#executeButton").click(function(event) {
-        $.ajax({
-            type: "POST",
-            url: "/executor.groovy",
-            data: { script: editor.getCode() },
-            dataType: "json",
-            complete: function (xhr, status) {
-                if (status === 'error' || !xhr.responseText) {
-                    alert("Error interacting with the Groovy web console server: " + status);
-                } else {
-                    var data;
-                    try {
-                        data = $.parseJSON(xhr.responseText);
-                    } catch (e) {
-                        alert("Impossible to parse JSON response: " + e);
-                        data = {executionResult: "", outputText: "", stacktraceText: ""};
-                    }
-
-                    $('#output').text(data.outputText);
-                    $('#result').text(data.executionResult);
-                    $('#stacktrace').text(data.stacktraceText);
-
-                    if (data.outputText.length > 0) {
-                        $("#tabs").tabs('select', 1);
-                        $('#output').text(data.outputText).fadeIn();
-                    } else {
-                        $('#output').fadeOut();
-                    }
-
-                    if (data.executionResult.length > 0) {
-                        if (data.executionResult != "null") {
-                            $("#tabs").tabs('select', 0);
-                        }
-                        $('#result').text(data.executionResult).fadeIn();
-                    } else {
-                        $('#result').fadeOut();
-                    }
-
-                    if (data.stacktraceText.length > 0) {
-                        $("#tabs").tabs('select', 2);
-                        $('#stacktrace').text(data.stacktraceText).fadeIn();
-                    } else {
-                        $('#stacktrace').fadeOut();
-                    }
-                }
-            }
-        });
-    });
+    $("#executeButton").click(executeScript);
 
     $('#loadingDiv')
             .hide()
@@ -97,6 +50,53 @@ $(document).ready(function() {
             .ajaxStop(function() {
                 $(this).hide();
             });
-
-
 });
+
+function executeScript() {
+    $.ajax({
+        type: "POST",
+        url: "/executor.groovy",
+        data: { script: editor.getCode() },
+        dataType: "json",
+        complete: function (xhr, status) {
+            if (status === 'error' || !xhr.responseText) {
+                alert("Error interacting with the Groovy web console server: " + status);
+            } else {
+                var data;
+                try {
+                    data = $.parseJSON(xhr.responseText);
+                } catch (e) {
+                    alert("Impossible to parse JSON response: " + e);
+                    data = {executionResult: "", outputText: "", stacktraceText: ""};
+                }
+
+                $('#output').text(data.outputText);
+                $('#result').text(data.executionResult);
+                $('#stacktrace').text(data.stacktraceText);
+
+                if (data.outputText.length > 0) {
+                    $("#tabs").tabs('select', 1);
+                    $('#output').text(data.outputText).fadeIn();
+                } else {
+                    $('#output').fadeOut();
+                }
+
+                if (data.executionResult.length > 0) {
+                    if (data.executionResult != "null") {
+                        $("#tabs").tabs('select', 0);
+                    }
+                    $('#result').text(data.executionResult).fadeIn();
+                } else {
+                    $('#result').fadeOut();
+                }
+
+                if (data.stacktraceText.length > 0) {
+                    $("#tabs").tabs('select', 2);
+                    $('#stacktrace').text(data.stacktraceText).fadeIn();
+                } else {
+                    $('#stacktrace').fadeOut();
+                }
+            }
+        }
+    });
+};
